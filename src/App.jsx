64082dbc015230c8faa33b48c1107223e18f3980
Shreyas Manolkar <shreyasmanolkar123@gmail.com>
2022-08-03 03:15:13 +0530

    initial commit v1

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import { Routes, Route, Link } from 'react-router-dom';
+import { Layout, Typography, Space } from 'antd';
+
+import { NavBar, Exchanges, Cryptocurrencies, CryptoDetails, News, HomePage } from './components';
+import './App.css';
+
+function App() {
+  return (
+    <div className='app'>
+      <div className='navbar'>
+        <NavBar />
+      </div>
+      <div className='main'>
+        <Layout>
+          <div className='routes'>
+            <Routes>
+              <Route exact path='/' element={<HomePage/>}/>
+              <Route exact path='/exchanges' element={<Exchanges/>}/>
+              <Route exact path='/cryptocurrencies' element={<Cryptocurrencies/>}/>
+              <Route exact path='/crypto/:coinId' element={<CryptoDetails/>}/>
+              <Route exact path='/news' element={<News/>}/>
+            </Routes>
+          </div>
+        </Layout>
+        <div className='footer'>
+          <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>
+            CryptoMudra <br />
+            All rights reserved
+          </Typography.Title>
+          <Space>
+            <Link to="/">Home</Link>
+            <Link to="/exchanges">Exchanges</Link>
+            <Link to="/news">News</Link>
+          </Space>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
